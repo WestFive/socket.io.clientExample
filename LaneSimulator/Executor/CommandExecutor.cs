6bbd69ff420dd9c51dbfa9987f556fc8dbc25a73
李辰龙 <1844424344@qq.com>
2018-06-08 10:13:17 +0800

    "C# Client"

--- LaneSimulator/Executor/CommandExecutor.cs
+++ LaneSimulator/Executor/CommandExecutor.cs
@@ -65,9 +65,7 @@ namespace LaneSimulator.Executor
                     break;
                 case "requestReleaseRuleExecutor":
                     command.commandName = "收到请求放行规则指令";
-                    commandExecutorResult?.Invoke(command);// 回调给主窗体打印日志
-                    //jobQueueDTO jobqueuedto = JsonConvert.DeserializeObject<jobQueueDTO>(File.ReadAllText(Application.StartupPath + "/conf/jobQueueDTO.json"));//读取默认jobQueueDTO
-                    //jobqueuedto = setJobQueueDTO(workingQueue, jobqueuedto);//将当前workingQueue的                    
+                    commandExecutorResult?.Invoke(command);// 回调给主窗体打印日志                
                     jobQueueDTO jobqueueDto = jobqueueMapper.toDto(workingQueue);
                     command.commandName = "解析jobQueudto完成,准备调用API";
                     commandExecutorResult?.Invoke(command); //回调给主窗体打印日志
@@ -78,12 +76,6 @@ namespace LaneSimulator.Executor
                     commandExecutorResult?.Invoke(command); //回调给主窗体打印日志
                     commandExecutorResult?.Invoke(workingQueue);//回调给主窗体更新workingQueue推送
                     break;
-                //case "test":
-                //    jobQueueDTO jobqueuedtoTest = JsonConvert.DeserializeObject<jobQueueDTO>(File.ReadAllText(Application.StartupPath + "/conf/jobQueueDTO.json"));//读取默认jobQueueDTO
-                //    JobQueue jobqueueTest = JsonConvert.DeserializeObject<JobQueue>(JsonConvert.SerializeObject(jobqueuedtoTest));
-
-                //    jobQueueDTO jobqueuedtoTest1 = JsonConvert.DeserializeObject<jobQueueDTO>(JsonConvert.SerializeObject(workingQueue));
-                //    break;
                 case "manualRelease":
                     dynamic commandParam = JsonConvert.DeserializeObject<Dictionary<string,string>>(command.parameter.ToString());
                     workingQueue.releaseResult = commandParam["releaseResult"];
@@ -102,74 +94,5 @@ namespace LaneSimulator.Executor
                     break;
             }
         }
-
-        //private jobQueueDTO setJobQueueDTO(JobQueue workingQueue, jobQueueDTO jobQueueDTO)
-        //{
-
-
-        //    jobQueueDTO.laneCode = workingQueue.laneCode;
-        //    jobQueueDTO.jobQueueCode = workingQueue.jobQueueCode;
-        //    jobQueueDTO.laneName = workingQueue.laneName;
-        //    jobQueueDTO.isOcrLicensePlateNumberSucceeded = workingQueue.isOcrLicensePlateNumberSucceeded;
-        //    jobQueueDTO.ocrLicensePlateNumber = workingQueue.ocrLicensePlateNumber;
-        //    jobQueueDTO.rfidCardNumber = workingQueue.rfidCardNumber;
-        //    jobQueueDTO.icCardNumber = workingQueue.icCardNumber;            
-        //    jobQueueDTO.customsLockNumber = workingQueue.customsLockNumber;
-        //    jobQueueDTO.licensePlateNumber = workingQueue.licensePlateNumber;
-        //    jobQueueDTO.processes = null;
-        //    jobQueueDTO.businessType = workingQueue.businessType;
-        //    jobQueueDTO.containerQuantity = workingQueue.containerQuantity;
-        //    jobQueueDTO.customsLockNumber = workingQueue.customsLockNumber;
-        //    jobQueueDTO.direction = workingQueue.direction;
-        //    jobQueueDTO.releaseResult = workingQueue.releaseResult;
-        //    List<ReleaseRuleResultDTO> list = new List<ReleaseRuleResultDTO>();
-        //    foreach (var item in jobQueueDTO.releaseRuleResults)
-        //    {
-        //        ReleaseRuleResultDTO dto = new ReleaseRuleResultDTO();
-        //        dto.id = item.id;
-        //        dto.jobQueueCode = item.jobQueueCode;
-        //        dto.jobQueueId = item.jobQueueId;
-        //        dto.releaseResultMessage = item.releaseResultMessage;
-        //        dto.releaseRuleName = item.releaseRuleName;
-        //        dto.releaseRuleStatus = item.releaseRuleStatus;
-        //        dto.sequence = item.sequence;
-        //        dto.updateTime = DateTime.UtcNow.ToString("s") + "Z"; 
-        //        list.Add(dto);
-
-        //    }
-        //    jobQueueDTO.releaseRuleResults = list;
-        //    jobQueueDTO.releasedType = workingQueue.releasedType;
-        //    jobQueueDTO.releasedBy = workingQueue.releasedBy;
-        //    jobQueueDTO.containers = null;
-        //    jobQueueDTO.endTime = DateTime.UtcNow.ToString("s") + "Z";
-        //    jobQueueDTO.extras = null;
-        //    jobQueueDTO.pictures = null;
-        //    return jobQueueDTO;
-        //}
-
-        //private void setQueue(jobQueueDTO jobqueuedto, JobQueue workingQueue)
-        //{
-        //    workingQueue.businessType = jobqueuedto.businessType;
-        //    workingQueue.containerQuantity = jobqueuedto.containerQuantity;
-        //    workingQueue.customsLockNumber = jobqueuedto.customsLockNumber;
-        //    workingQueue.direction = jobqueuedto.direction;
-        //    workingQueue.releaseResult = jobqueuedto.releaseResult;
-        //    workingQueue.releaseRuleResults = JsonConvert.DeserializeObject<List<ReleaseRuleResult>>(JsonConvert.SerializeObject(jobqueuedto.releaseRuleResults));
-        //    workingQueue.releasedType = jobqueuedto.releasedType;
-        //    workingQueue.releasedBy = jobqueuedto.releasedBy;
-        //    workingQueue.containers = JsonConvert.DeserializeObject<List<Container>>(JsonConvert.SerializeObject(jobqueuedto.containers));
-        //    workingQueue.endTime = jobqueuedto.endTime;
-        //    workingQueue.extras = jobqueuedto.extras;
-        //    workingQueue.laneCode = jobqueuedto.laneCode;
-        //    workingQueue.jobQueueCode = jobqueuedto.jobQueueCode;
-        //    workingQueue.laneName = jobqueuedto.laneName;
-        //    workingQueue.isOcrLicensePlateNumberSucceeded = jobqueuedto.isOcrLicensePlateNumberSucceeded;
-        //    workingQueue.ocrLicensePlateNumber = jobqueuedto.ocrLicensePlateNumber;
-        //    workingQueue.rfidCardNumber = jobqueuedto.rfidCardNumber;
-        //    workingQueue.icCardNumber = jobqueuedto.icCardNumber;
-        //    workingQueue.customsLockNumber = jobqueuedto.customsLockNumber;
-        //    workingQueue.licensePlateNumber = jobqueuedto.licensePlateNumber;                                       
-
-        //}
     }
 }
